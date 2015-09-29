914dc9c7fcf6b888db901d35f33e9e90eb2cdcc6
colin-h <colinharman@utexas.edu>
2015-09-29 16:40:46 -0500

    style changes and cleanup

--- public/javascripts/image_render.js
+++ public/javascripts/image_render.js
@@ -1,9 +1,7 @@
 function placeImages() {
 
   var pageDefaults = {
-
     crop: 'fit',
-
   };
 
   var $paragraphs = $('.mainText > p');
@@ -11,22 +9,18 @@ function placeImages() {
   var $containers = $('#images > .image-container');
   var imgSpread = Math.floor($paragraphs.length / $containers.length);
 
+  //format and place images with help of imgix
   $('.mainText p:nth-child('+ imgSpread +'n)').each(function (i) {
 
     $(this).after($containers.get(i));
-    console.log($(this).width());
     var image = $images.get(i);
     var myImage = new imgix.URL('' + image.src);
 
-    console.log(image);
-    console.log(myImage);
     myImage.setParams(pageDefaults);
     myImage.setHeight(500);
     myImage.setWidth($('.mainText').width());
     myImage.attachImageTo($containers.get(i));
 
-    // $(this).after($images.get(i));
-    $(this).after(myImage);
   });
 
   $('#images').empty();
@@ -34,7 +28,6 @@ function placeImages() {
 
 
 $(document).ready(function(){
-  console.log("The doc is ready!");
   imgix.onready(function() {
     placeImages();
   });
--- server.js
+++ server.js
@@ -1,45 +1,21 @@
-// var connect = require('connect');
-// var express = require('express');
-// var http = require('http');
-// var app = connect();
-
-// app.use(express.static('client'));
-
-// var bodyParser = require('body-parser');
-// app.use(bodyParser());
-
-// app.post('/fetchid', function(req, res) {
-//   console.log(req.body);
-// });
-
-// // app.use('/fetchId', function (req, res, next) {
-// //   console.log(req.body);
-// //   res.end("nice search on : ", req.body);
-// //   // console.log(req.body.article);
-// // });
-
-
-
-
-
-var express = require('express')
-var http = require('http')
-var bodyParser = require('body-parser')
-var request = require('request')
-var jade = require('jade')
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var request = require('request');
+var jade = require('jade');
 var fs = require('fs');
 
-var app = express()
+var app = express();
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
-app.set('stylesheets', __dirname + '/vendor/foundation/css')
-app.set('views', __dirname + '/views')
+app.set('stylesheets', __dirname + '/vendor/foundation/css');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
@@ -48,10 +24,10 @@ app.get('/', function(req, res) {
 
 // POST /fetchId gets urlencoded bodies
 app.post('/:fetchId', urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
+  if (!req.body) return res.sendStatus(400);
   console.log(req.body);
 
-
+  //send request to tmc-api
   request('https://tmc-api-v4.herokuapp.com/v4/article/' + req.body.id, function (error, response, body) {
     if (!error) {
       var data = JSON.parse(body);
@@ -61,15 +37,7 @@ app.post('/:fetchId', urlencodedParser, function (req, res) {
     } else {
       return console.log(error);
     }
+  });
+});
 
-  })
-
-  // res.send(article);
-})
-// var server = app.listen(3000, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
-
-//   console.log('Example app listening at http://%s:%s', host, port);
-// });
-http.createServer(app).listen(3000)
+http.createServer(app).listen(3000);
