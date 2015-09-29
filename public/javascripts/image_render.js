de894e144a7e3f70b48749e1956b444db4cb3e7a
colin-h <colinharman@utexas.edu>
2015-09-29 15:31:54 -0500

    basic template complete, images consistent

--- public/javascripts/image_render.js
+++ public/javascripts/image_render.js
@@ -1,18 +1,41 @@
 function placeImages() {
 
+  var pageDefaults = {
+
+    crop: 'fit',
+
+  };
+
   var $paragraphs = $('.mainText > p');
   var $images = $('#images > img');
-  var imgSpread = Math.floor($paragraphs.length / $images.length);
+  var $containers = $('#images > .image-container');
+  var imgSpread = Math.floor($paragraphs.length / $containers.length);
 
   $('.mainText p:nth-child('+ imgSpread +'n)').each(function (i) {
-      $(this).after($images.get(i));
+
+    $(this).after($containers.get(i));
+    console.log($(this).width());
+    var image = $images.get(i);
+    var myImage = new imgix.URL('' + image.src);
+
+    console.log(image);
+    console.log(myImage);
+    myImage.setParams(pageDefaults);
+    myImage.setHeight(500);
+    myImage.setWidth($('.mainText').width());
+    myImage.attachImageTo($containers.get(i));
+
+    // $(this).after($images.get(i));
+    $(this).after(myImage);
   });
+
+  $('#images').empty();
 }
 
 
 $(document).ready(function(){
   console.log("The doc is ready!");
-
-  placeImages();
-
+  imgix.onready(function() {
+    placeImages();
+  });
 });
